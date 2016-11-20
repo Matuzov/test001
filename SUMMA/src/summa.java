f275ce578e0d13493ab6e96fa9a23b4f2fe12f21
Matuzov <maxim.matuzov@yandex.ru>
2016-11-20 07:53:35 +0400

    first post
    Публикация папки SUMMA

---
+++ SUMMA/src/summa.java
@@ -0,0 +1,22 @@
+
+class summa {
+public static void main (String args[]) {
+	int i = 35;
+	int k = 35;
+	k = count(k);
+	
+	System.out.print("Xage - ");
+	System.out.println(i);
+	//System.in.readln();
+	System.out.print("current age - ");
+	System.out.println(k);
+	
+	
+}
+
+private static int count(int x) {
+	// TODO Auto-generated method stub
+	x=x+1;
+	return x;
+}
+}
